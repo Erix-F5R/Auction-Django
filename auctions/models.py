36437e7fa824ee8d33bb58cee5cd839116364922
Eric <ericpeter.f@gmail.com>
2022-02-11 16:08:12 -0500

    Model architecture

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Auction(models.Model):
+    title = models.CharField(max_length=100)
+    min_bid = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Bid(models.Model):
+    
+    amount = models.FloatField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
